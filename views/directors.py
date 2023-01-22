cf5c22fea40ece14e9062e192dcd107eda94c8e0
nick <7zx.1zx@gmail.com>
2023-01-22 11:47:13 +0300

    adde hashing methods;  updated directirs and genres CBVs(2)

--- views/directors.py
+++ views/directors.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from flask import request
 from dao.model.director import DirectorSchema
 from implemented import director_service
 
@@ -13,6 +13,11 @@ class DirectorsView(Resource):
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        request_json = request.json
+        director_service.create(request_json)
+        return '', 201
+
 
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
@@ -20,3 +25,16 @@ class DirectorView(Resource):
         r = director_service.get_one(rid)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
+
+    def put(self, rid):
+        request_json = request.json
+        if rid not in request_json:
+            request_json['rid'] = rid
+
+        director_service.update(request_json)
+        return '', 204
+
+    def delete(self, rid):
+        director_service.delete(rid)
+        return '', 204
+
--- views/genres.py
+++ views/genres.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from flask import request
 from dao.model.genre import GenreSchema
 from implemented import genre_service
 
@@ -13,6 +13,11 @@ class GenresView(Resource):
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        request_json = request.json
+        genre_service.create(request_json)
+        return '', 201
+
 
 @genre_ns.route('/<int:rid>')
 class GenreView(Resource):
@@ -20,3 +25,15 @@ class GenreView(Resource):
         r = genre_service.get_one(rid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    def put(self, rid):
+        request_json = request.json
+        if rid not in request_json:
+            request_json['rid'] = rid
+
+        genre_service.update(request_json)
+        return '', 204
+
+    def delete(self, rid):
+        genre_service.delete(rid)
+        return '', 204
